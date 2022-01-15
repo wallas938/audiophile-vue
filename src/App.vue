36cfff75083dc82c60d27d77f82d362b6b25e52e
Wallas9_3 <drame.sissako001@gmail.com>
2022-01-15 01:20:15 +0100

    the project is ready for dev

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
+<template>
+  <p>TEST</p>
+</template>
+
 <script>
 export default {
   name: "App",
@@ -5,5 +9,8 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+p {
+  font-family: 'Manrope', sans-serif;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+/** Sass files import */
+import './sass/_reset.scss';
+import './sass/_global.scss';
+import './sass/_mixins.scss';
+import './sass/_helpers.scss';
+
 createApp(App).mount('#app')
